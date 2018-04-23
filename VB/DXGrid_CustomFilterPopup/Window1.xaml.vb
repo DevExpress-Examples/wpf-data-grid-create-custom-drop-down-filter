09a6984c5eb007957af02bb32c7665001618979a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DXGrid_CustomFilterPopup/Window1.xaml.vb
@@ -0,0 +1,65 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Markup
+Imports DevExpress.Data.Filtering
+
+Namespace DXGrid_CustomFilterPopup
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = GridData.GetData()
+		End Sub
+	End Class
+	Public Class GridData
+		Public Shared Function GetData() As List(Of DataObject)
+			Dim data As New List(Of DataObject)()
+			For i As Integer = 0 To 99
+				data.Add(New DataObject() With {.Index = i})
+			Next i
+			Return data
+		End Function
+	End Class
+	Public Class DataObject
+		Private privateIndex As Integer
+		Public Property Index() As Integer
+			Get
+				Return privateIndex
+			End Get
+			Set(ByVal value As Integer)
+				privateIndex = value
+			End Set
+		End Property
+	End Class
+	Public Class IntToCriteriaOperatorConverter
+		Inherits MarkupExtension
+		Implements IValueConverter
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+		#Region "IValueConverter Members"
+		Private Function IValueConverter_Convert(ByVal value As Object, _ 
+				ByVal targetType As Type, ByVal parameter As Object, _
+				ByVal culture As System.Globalization.CultureInfo) _
+				As Object Implements IValueConverter.Convert
+			Dim op As BinaryOperator = TryCast(value, BinaryOperator)
+			If Object.ReferenceEquals(op, Nothing) Then
+				Return Nothing
+			End If
+			Dim operandValue As OperandValue = TryCast(op.RightOperand, OperandValue)
+			Return operandValue.Value
+		End Function
+		Private Function ConvertBack(ByVal value As Object, ByVal targetType As Type, _
+				ByVal parameter As Object, _
+				ByVal culture As System.Globalization.CultureInfo) _
+				As Object Implements IValueConverter.ConvertBack
+			Return New BinaryOperator("Index", Convert.ToInt32(value), _
+				BinaryOperatorType.Greater)
+		End Function
+		#End Region
+	End Class
+End Namespace
