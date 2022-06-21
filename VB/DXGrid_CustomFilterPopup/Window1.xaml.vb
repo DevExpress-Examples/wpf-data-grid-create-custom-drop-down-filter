5fabb035fc6eaa90055df5b0a2b2ff868ac564b0
DevExpressExampleBot <fakeEmail>
2022-06-21 16:55:48 +0400

    Source auto update [skip ci]

--- VB/DXGrid_CustomFilterPopup/Window1.xaml.vb
+++ VB/DXGrid_CustomFilterPopup/Window1.xaml.vb
@@ -1,51 +1,49 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports DevExpress.Data.Filtering
+Imports System
+Imports System.Collections.ObjectModel
 Imports System.Windows
 Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Markup
-Imports DevExpress.Data.Filtering
 
 Namespace DXGrid_CustomFilterPopup
-	Partial Public Class Window1
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = GridData.GetData()
-		End Sub
-	End Class
-	Public Class GridData
-		Public Shared Function GetData() As List(Of DataObject)
-			Dim data As New List(Of DataObject)()
-			For i As Integer = 0 To 99
-				data.Add(New DataObject() With {.Index = i})
-			Next i
-			Return data
-		End Function
-	End Class
-	Public Class DataObject
-		Public Property Index() As Integer
-	End Class
-	Public Class IntToCriteriaOperatorConverter
-		Inherits MarkupExtension
-		Implements IValueConverter
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-			Return Me
-		End Function
-		#Region "IValueConverter Members"
-		Private Function IValueConverter_Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-			Dim op As BinaryOperator = TryCast(value, BinaryOperator)
-			If Object.ReferenceEquals(op, Nothing) Then
-				Return Nothing
-			End If
-			Dim operandValue As OperandValue = TryCast(op.RightOperand, OperandValue)
-			Return operandValue.Value
-		End Function
-		Private Function IValueConverter_ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-			Return New BinaryOperator("Index", Convert.ToInt32(value), BinaryOperatorType.Greater)
-		End Function
-		#End Region
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = GridData.GetData()
+        End Sub
+    End Class
+
+    Public Class GridData
+
+        Shared Public Function GetData() As ObservableCollection(Of DataObject)
+            Dim data As ObservableCollection(Of DataObject) = New ObservableCollection(Of DataObject)()
+            For i As Integer = 0 To 100 - 1
+                data.Add(New DataObject() With {.Index = i})
+            Next
+
+            Return data
+        End Function
+    End Class
+
+    Public Class DataObject
+
+        Public Property Index As Integer
+    End Class
+
+    Public Class IntToCriteriaOperatorConverter
+        Implements IValueConverter
+
+        Private Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+            Dim binaryOperator As BinaryOperator = TryCast(value, BinaryOperator)
+            If ReferenceEquals(binaryOperator, Nothing) Then Return Nothing
+            Dim operandValue As OperandValue = TryCast(binaryOperator.RightOperand, OperandValue)
+            Return operandValue.Value
+        End Function
+
+        Private Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Return New BinaryOperator("Index", System.Convert.ToInt32(value), BinaryOperatorType.GreaterOrEqual)
+        End Function
+    End Class
 End Namespace
